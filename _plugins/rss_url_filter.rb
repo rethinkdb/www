0bc9c5b3038a9732586beb3ae91dd66192bb5930
Michael Glukhovsky <mglukhovsky@gmail.com>
2015-06-15 21:44:20 -0700

    Added Jekyll plugin: Liquid filter to convert relative URLs to absolute URLS based on rethinkdb.com. Updated feed.xml to use the filter for URLs.

---
+++ _plugins/rss_url_filter.rb
@@ -0,0 +1,11 @@
+module Jekyll
+  module RSSURLFilter
+    def relative_urls_to_absolute(input)
+      # Root of the absolute url (we'll prepend this to each relative link)
+      url = "http://rethinkdb.com"
+      input.gsub('src="/', 'src="' + url + '/').gsub('href="/', 'href="' + url + '/')
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::RSSURLFilter)
